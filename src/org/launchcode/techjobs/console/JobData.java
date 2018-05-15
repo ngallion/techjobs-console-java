76ecbde371d9b54d792ec0f2991977dac4d9c7ba
Nick Gallion <nicholas.gallion@gmail.com>
2018-05-15 14:45:15 -0500

    Added search functionality

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -84,6 +84,27 @@ public class JobData {
         return jobs;
     }
 
+    public static ArrayList<HashMap<String, String>> findByValue(String search) {
+
+        // load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> job: allJobs) {
+            for (String value: job.values()){
+
+                if (value.toLowerCase().contains(search.toLowerCase())) {
+                    if (!jobs.contains(job)) {
+                        jobs.add(job);
+                    }
+                }
+            }
+        }
+
+        return jobs;
+    }
+
     /**
      * Read in data from a CSV file and store it in a list
      */
